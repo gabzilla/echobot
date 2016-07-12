927a148a461c7f87c4cc15f6398b8df51b21062f
Gabby <gabzilla@users.noreply.github.com>
2016-07-12 16:25:00 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// index.js
+require('dotenv').config();
+var token = 'API_TOKEN'
+var Slack = require('@slack/client');
+var RtmClient = Slack.RtmClient;
+var RTM_EVENTS = Slack.RTM_EVENTS;
+
+var token = process.env.API_TOKEN;
+
+var rtm = new RtmClient(token, { logLevel: 'info' });
+rtm.start();
+
+rtm.on(RTM_EVENTS.MESSAGE, function(message) {
+  var channel = message.channel;
+  var text = message.text;
+  rtm.sendMessage(text, channel);
+});
